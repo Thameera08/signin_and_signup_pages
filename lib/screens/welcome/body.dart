e5f2f3f1403198513b6289a3d6347a41f3790500
Thameera Dananjaya <84010024+Thameera08@users.noreply.github.com>
2023-07-04 15:50:07 +0530

    commit 3

--- lib/screens/welcome/body.dart
+++ lib/screens/welcome/body.dart
@@ -7,8 +7,22 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-      height: size.height,
-      width: size.width,
+        height: size.height,
+        width: double.infinity,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(
+            "asset/images/main_top.png",
+            width: size.width * 0.3,
+         ),
+        ),
+        Positioned(child: Image.asset("asset/images/main_bottom.png"))
+       ],
+      ),
     );
   }
 }
