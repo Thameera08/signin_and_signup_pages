9f9ab1371a98209c93811ef2e0a20dee1ee05e5b
Thameera Dananjaya <84010024+Thameera08@users.noreply.github.com>
2023-07-13 14:59:28 +0530

    commit 1

--- lib/screens/welcome/components/body.dart
+++ lib/screens/welcome/components/body.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:signin_and_signup_pages/screens/welcome/components/background.dart';
 
-
 class Body extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -14,8 +13,11 @@ class Body extends StatelessWidget {
           Text(
             "Welcome to Train Ride",
             style: TextStyle(fontWeight: FontWeight.bold),
-         ),
-         SvgPicture.asset("")
+          ),
+          SvgPicture.asset(
+            "assets/images/train.svg",
+            height: size.height * 0.4, // Adjust the height as needed
+          ),
         ],
       ),
     );
